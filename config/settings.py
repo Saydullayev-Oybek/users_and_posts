255442847b7b8da10a20f972e3cc13b9b2634177
oybek.john <oybek.john01@gmail.com>
2023-02-04 22:14:58 +0500

    password_reset_form

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
